3d9ffef43ccea348d6f342989727c68415d2efb3
Maltzael <darkazazel1995@gmail.com>
2023-01-03 14:19:11 +0100

    golang: added custom oauth for lambda 3 from apigateway, lambda return apigateway response with jwt token in body

--- Go AWS/go.mod
+++ Go AWS/go.mod
@@ -5,5 +5,6 @@ go 1.17
 require (
 	github.com/aws/aws-lambda-go v1.36.0 // indirect
 	github.com/aws/aws-sdk-go v1.44.160 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
--- Go AWS/source/lambda_receiver_api/lambda_RC_api.go
+++ Go AWS/source/lambda_receiver_api/lambda_RC_api.go
@@ -2,28 +2,79 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
-//type Postinput struct {
-//	Fruit string `json:"fruit"`
-//	Size  string `json:"size"`
-//	Color string `json:"color"`
-//	Key1  string `json:"key1"`
-//}
+type PostInput struct {
+	User  *string `json:"User"`
+	Email *string `json:"Email"`
+	Data  *string `json:"Data"`
+}
+type JWTClaim struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	jwt.StandardClaims
+}
 
-func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("event.Body %v\n", request.Body)
-	body := fmt.Sprintf("Hello from labmda! Body: %v\n", request.Body)
+var jwtKey = []byte("supersecretkey")
+
+func GenerateJWT(email string, username string) (tokenString string, err error) {
+	expirationTime := time.Now().Add(1 * time.Hour)
+	claims := &JWTClaim{
+		Email:    email,
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err = token.SignedString(jwtKey)
+	return
+}
+func ValidateToken(signedToken string) (err error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JWTClaim{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(jwtKey), nil
+		},
+	)
+	if err != nil {
+		return
+	}
+	claims, ok := token.Claims.(*JWTClaim)
+	if !ok {
+		err = errors.New("couldn't parse claims")
+		return
+	}
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		err = errors.New("token expired")
+		return
+	}
+	return
+}
+
+func lambdaMain(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var postInput PostInput
+	err := json.Unmarshal([]byte(request.Body), &postInput)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	fmt.Printf(*postInput.Data)
+	token, _ := GenerateJWT(*postInput.Email, *postInput.User)
+	err = ValidateToken(token)
 	return events.APIGatewayProxyResponse{
-		Body:       body,
+		Body:       token,
 		StatusCode: 200,
-	}, nil
+	}, err
 }
 
 func main() {
-	lambda.Start(handleRequest)
-
+	lambda.Start(lambdaMain)
 }
